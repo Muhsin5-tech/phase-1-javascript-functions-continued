7eb8556de56276c8af67559b70823b1440244300
Muhsin Ali <muhsin.ali@student.moringaschool.com>
2024-09-29 12:56:55 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+// index.js
+
+// Function declaration for saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function expression for mondayWork
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Function to wrap an adjective
+  function wrapAdjective(wrapper) {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+  
+  // Exporting functions
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+  };
+  
